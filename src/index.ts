3685f10be5ff66d7ce6f602148bda1e640c74bfd
Jamil Omar <jfalconi63@hotmail.com>
2020-03-31 19:17:34 -0400

    fix: snyk

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export {LbServicesAuthComponent} from './component';
 // alias for chaging from LbServicesAuthComponent -> ServicesAuthComponent
-export {LbServicesAuthComponent as ServicesAuthComponent } from './component';
+export {LbServicesAuthComponent as ServicesAuthComponent} from './component';
 export * from './keys';
 export * from './decorators';
